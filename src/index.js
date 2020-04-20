b156a24a76ccc9b1d52a6239eec6b94ae7eb12b1
alexjball <8595776+alexjball@users.noreply.github.com>
2020-04-19 20:09:05 -0400

    Use HashRouter so it works with GH Pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
