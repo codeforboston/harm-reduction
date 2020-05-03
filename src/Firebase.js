57170142b1454b8180589a639d605fa0ea726b8c
Alex Ball <aball@asus.lan>
2020-05-03 14:15:38 -0400

    Add firebase auth dependencies

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
+
+import 'firebase/auth';
 import 'firebase/firestore';
 
 firebase.initializeApp({
@@ -13,3 +15,4 @@ firebase.initializeApp({
 });
 
 export const db = firebase.firestore();
+export const auth = firebase.auth();
