10f9f41b7e186c22fd6fdecefbcbf846da2c7081
alexjball <8595776+alexjball@users.noreply.github.com>
2020-05-24 23:06:40 -0400

    Use browser router

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
