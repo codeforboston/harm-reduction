f884915e7debf440d2cd08766f2ed3ca93ead85d
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-27 03:47:25 -0400

    add table to engagement page

--- src/Engagements.js
+++ src/Engagements.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { P } from './widgets';
 import RecordEngagementForm from './RecordEngagementForm';
+import EngagementTable from './EngagementTable';
 
 export default () => {
   return (
     <>
+      <div>
+        <P>
+          Engagements are interactions between WROC and a participant. Overdose
+          outreach responses are recorded as engagements, as are interactions at
+          drop-in hours.
+        </P>
+        <RecordEngagementForm />
+      </div>
       <P>
-        Engagements are interactions between WROC and a participant. Overdose
-        outreach responses are recorded as engagements, as are interactions at
-        drop-in hours.
+        Once a participant is added to the system, they will appear in the table
+        below.
       </P>
-      <RecordEngagementForm />
+      <EngagementTable />
     </>
   );
 };
