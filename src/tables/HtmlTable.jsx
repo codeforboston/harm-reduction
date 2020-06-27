41c763a75230ba509f49ae614431db2e838948ed
Charlie Kelly <charliekelly31415@gmail.com>
2020-06-27 03:44:34 -0400

    remove conversion - is default

--- src/tables/HtmlTable.jsx
+++ src/tables/HtmlTable.jsx
@@ -1,51 +1,25 @@
 import React from 'react';
 
-/*
-columns = [{
-  dataField: 'id',
-  text: 'ID',
-}] */
-
-/**
- * format metadata for current table
- */
-const convertColumn = ({
-  fieldName,
-  labelName,
-  isKey = false,
-  sortable = true,
-  display = true,
-}) => ({
-  dataField: fieldName,
-  text: labelName,
-  visible: display,
-});
-
-const transformFields = fields => fields.map(field => convertColumn(field));
-
 /**
  * rows: data
  * @param {*} props
  */
-const HtmlTable = ({ fields, rows }) => {
-  const columns = transformFields(fields);
+const HtmlTable = ({ columns, rows }) => {
   return (
     <table>
       <thead>
         <tr>
-          {columns.map(
-            (column, index) =>
-              column.visible && <th key={index}>{column.text}</th>
-          )}
+          {columns.map((column, index) => (
+            <th key={index}>{column.label}</th>
+          ))}
         </tr>
       </thead>
       <tbody>
         {rows.map(row => (
           <tr key={row.id}>
-            {columns.map(
-              (column, index) =>
-                column.visible && <td key={index}>{row[column.dataField]}</td>
-            )}
+            {columns.map((column, index) => (
+              <td key={index}>{row[column.name]}</td>
+            ))}
           </tr>
         ))}
       </tbody>
